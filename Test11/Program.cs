e727ab59534c8575411545cda07bfc288fbb5978
USER <cuxdi@yandex.ru>
2022-08-20 17:44:13 +0400

    задача с семинара

--- Test11/Program.cs
+++ Test11/Program.cs
@@ -1,22 +1,13 @@
-﻿// 18 Выводим номер четверти по данным x y.
+﻿// 18 Вводим координаты двух точек и находим расстояние между ними в 2д пространстве.
 using static System.Console;
 Clear();
-Write("Ведите число x:");
-int x = Convert.ToInt32(ReadLine());
-Write("Ведите число y:");
-int y = Convert.ToInt32(ReadLine());
-if(x > 0 && y > 0)
-{
-    Write("1");
-}
-if(x > 0 && y < 0)
-{
-    Write("2");
-}
-if(x < 0 && y < 0)
-{
-    Write("3");
-}
-if(x < 0 && y > 0)
-{
-    Write("4");
+WriteLine("Ведите координаты первой точки:");
+int x1 = Convert.ToInt32(ReadLine());
+int y1 = Convert.ToInt32(ReadLine());
+
+WriteLine("Введите координаты второй точки:");
+int x2 = Convert.ToInt32(ReadLine());
+int y2 = Convert.ToInt32(ReadLine());
+
+int distance = Convert.ToInt32(Math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)));
+Write($"Расстояние между точками равно {distance}");
