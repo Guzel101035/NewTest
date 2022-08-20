d7fde82ea2034ba2c4193bac055645c5ec2790c1
USER <cuxdi@yandex.ru>
2022-08-20 18:33:57 +0400

    задачаТаблицаКвадратов

---
+++ Test12/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу , которая принимает на входе число N 
+//и выдаёт таблицу квадратов чисел от 1 до N.
+
+using static System.Console;
+Clear();
+WriteLine("Ведите число :");
+int n = Convert.ToInt32(ReadLine());
+
+int index = 1;
+//int square = 0;
+//int result = 0;
+
+while(index <=n )
+{
+    Write(index*index + " ");
+    index++;
+}
+WriteLine();
