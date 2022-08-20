59bc188e7062b12be303e6aa3e437cd67a62ef56
USER <cuxdi@yandex.ru>
2022-08-20 13:19:28 +0400

    задача 17 выводим x y

---
+++ Test10/Program.cs
@@ -0,0 +1,35 @@
+﻿// 18 Вводим номер четверти выводим данным x y.
+using static System.Console;
+Clear();
+Write("Ведите число a:");
+int a = Convert.ToInt32(ReadLine());
+
+switch(a)
+{
+    case 1:
+    {
+          Write("x > 0 && y > 0");
+          break;
+    }
+    case 2:
+    {
+          Write("x > 0 && y < 0");
+          break;
+    }
+    case 3:
+    {
+          Write("x < 0 && y < 0");    
+          break;
+    }
+    case 4:
+    {
+           Write("x < 0 && y > 0");
+           break;
+    }
+    default:
+    {
+           Write("Нет такой четверти"); 
+           break; 
+    }
+}
+    
---
+++ Test11/Program.cs
@@ -0,0 +1,22 @@
+﻿// 18 Выводим номер четверти по данным x y.
+using static System.Console;
+Clear();
+Write("Ведите число x:");
+int x = Convert.ToInt32(ReadLine());
+Write("Ведите число y:");
+int y = Convert.ToInt32(ReadLine());
+if(x > 0 && y > 0)
+{
+    Write("1");
+}
+if(x > 0 && y < 0)
+{
+    Write("2");
+}
+if(x < 0 && y < 0)
+{
+    Write("3");
+}
+if(x < 0 && y > 0)
+{
+    Write("4");
