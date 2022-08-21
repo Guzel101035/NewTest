8b69bfb8f1f44fb3406aa0c18cc2c64a7a806418
USER <cuxdi@yandex.ru>
2022-08-21 09:46:13 +0400

    задача на кубы

---
+++ Test14.DZ/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу , которая принимает на входе число(N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1,8,27
+// 5 -> 1,8,27,64,125
+
+using static System.Console;
+Clear();
+WriteLine("Ведите число :");
+int n = Convert.ToInt32(ReadLine());
+
+int index = 1;
+//int square = 0;
+//int result = 0;
+
+while(index <=n )
+{
+    Write(index*index*index + " ");
+    index++;
+}
+WriteLine();
+
