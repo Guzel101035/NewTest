4c1017ed911d2f531595488608cae10a0d1d6611
USER <cuxdi@yandex.ru>
2022-08-21 10:58:00 +0400

    задача

--- Test11/Program.cs
+++ Test11/Program.cs
@@ -4,10 +4,12 @@ Clear();
 WriteLine("Ведите координаты первой точки:");
 int x1 = Convert.ToInt32(ReadLine());
 int y1 = Convert.ToInt32(ReadLine());
+int z1 = Convert.ToInt32(ReadLine());
 
 WriteLine("Введите координаты второй точки:");
 int x2 = Convert.ToInt32(ReadLine());
 int y2 = Convert.ToInt32(ReadLine());
+int z2 = Convert.ToInt32(ReadLine());
 
-double distance = Math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-Write($"Расстояние между точками равно {distance:f2}");
+double distance = Math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)+ (z2-z1)*(z2-z1));
+Write($"Расстояние между точками равно { distance:f2} ");
